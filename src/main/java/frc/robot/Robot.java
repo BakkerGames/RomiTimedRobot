314241a95b93657b3edae1abd9d6f10a738cfa7b
Scott Bakker <sbakker343@gmail.com>
2021-03-12 12:12:13 -0500

    Working Challenge 1 code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,54 +5,82 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.hal.SimDevice;
+import edu.wpi.first.hal.SimDevice.Direction;
+import edu.wpi.first.hal.SimDouble;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  static final String kDefaultAuto = "Default";
+  static final String kCustomAuto = "My Auto";
+  String m_autoSelected;
+  final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  private final RomiDrivetrain m_drivetrain = new RomiDrivetrain();
+  final RomiDrivetrain m_drivetrain = new RomiDrivetrain();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+XboxController xboxController = new XboxController(0);
+
+double driveY = 0.0;
+double driveX = 0.0;
+
+double leftEncoder = 0.0;
+double rightEncoder = 0.0;
+
+final double DEAD_ZONE = 0.2;
+final double SPEED_SCALE = 0.9;
+final double TURN_SCALE = 0.8;
+final double TURN_SPEED_MIN = 0.3;
+final double GRYO_BOUNDS = 2.0;
+final double TURNING_FACTOR = 0.04844;
+
+SimDouble gyro;
+double gyroOffset = 0.0;
+double gyroTurningScale = 90.0;
+double targetEncoder = 0.0;
+
+int autoStage = 1;
+
+boolean turningPressed = false;
+boolean driveStraightPressed = false;
+
+Timer timer = new Timer();
+
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+SimDevice gyroSimDevice = SimDevice.create("Gyro:RomiGyro");
+
+if (gyroSimDevice != null) {
+gyroSimDevice.createBoolean("init", Direction.kOutput, true);
+gyro = gyroSimDevice.createDouble("angle_z", Direction.kInput, 0.0);
+}
+
+timer.start();
+}
+
+@Override
+public void robotPeriodic() {
+leftEncoder = m_drivetrain.getLeftDistanceInch();
+rightEncoder = m_drivetrain.getRightDistanceInch();
+
+SmartDashboard.putNumber("Drive Y", driveY);
+SmartDashboard.putNumber("Drive X", driveX);
+SmartDashboard.putNumber("Left Encoder", leftEncoder);
+SmartDashboard.putNumber("Right Encoder", rightEncoder);
+SmartDashboard.putNumber("Angle", getGyroAngle());
+SmartDashboard.putNumber("Raw Angle", gyro.get());
+SmartDashboard.putNumber("Gryo Offset", gyroOffset);
+SmartDashboard.putNumber("Gryo Turning Scale", gyroTurningScale);
+SmartDashboard.putNumber("Auto Stage", autoStage);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {}
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
@@ -60,20 +88,70 @@ public class Robot extends TimedRobot {
     System.out.println("Auto selected: " + m_autoSelected);
 
     m_drivetrain.resetEncoders();
+gyroOffset = gyro.get();
+
+autoStage = 1;
+
+driveY = 0;
+driveX = 0;
+
+timer.reset();
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
+if (timer.get() < 1.0) {
+return;
+}
+
+switch(autoStage) {
+  case 1:
+  drive(13);
+  break;
+  case 2:
+  turn(-70);
+  break;
+  case 3:
+  drive(13.5);
+  break;
+  case 4:
+  turn(-88);
+  break;
+  case 5:
+  drive(6);
+  break;
+  case 6:
+  turn(-40);
+  break;
+  case 7:
+  drive(7);
+  break;
+  case 8:
+  turn(45);
+  break;
+  case 9:
+  drive(7);
+  break;
+  case 10:
+  turn(100);
+  break;
+  case 11:
+  drive(13.5);
+  break;
+  case 12:
+  turn(95);
+  break;
+  case 13:
+  drive(14);
         break;
-      case kDefaultAuto:
       default:
-        // Put default auto code here
+  driveY = 0.0;
+  driveX = 0.0;
         break;
     }
+
+m_drivetrain.arcadeDrive(driveY, driveX);
   }
 
   /** This function is called once when teleop is enabled. */
@@ -82,7 +160,75 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+public void teleopPeriodic() {
+if (xboxController.getYButton()) {
+if (driveStraightPressed == false) {
+gyroOffset = gyro.get();
+driveStraightPressed = true;
+}
+
+driveY = 0.8;
+} else if ((Math.abs(xboxController.getY(Hand.kLeft)) > DEAD_ZONE) &&
+!xboxController.getBButton() &&
+!xboxController.getXButton()){
+
+driveY = -xboxController.getY(Hand.kLeft) * SPEED_SCALE;
+driveStraightPressed = false;
+} else {
+driveY = 0.0;
+driveStraightPressed = false;
+}
+
+if (xboxController.getYButton()) {
+driveX = -(getGyroAngle() / 20);
+} else if (xboxController.getBButton()) {
+if (turningPressed == false) {
+gyroOffset = gyro.get();
+m_drivetrain.resetEncoders();
+targetEncoder = TURNING_FACTOR * 90;
+turningPressed = true;
+}
+
+if ((getGyroAngle() - 90.0) > -10.0) {
+driveX = 0;
+} else {
+driveX = -(((getGyroAngle() - 90.0) / 90.0) * 0.25) + 0.25;
+}
+
+} else if (xboxController.getXButton()) {
+if (turningPressed == false) {
+gyroOffset = gyro.get();
+m_drivetrain.resetEncoders();
+targetEncoder = TURNING_FACTOR * -90;
+turningPressed = true;
+}
+
+if ((getGyroAngle() + 90.0) < 10.0) {
+driveX = 0;
+} else {
+driveX = -(((getGyroAngle() + 90.0) / 90.0) * 0.25) - 0.25;
+}
+} else if (Math.abs(xboxController.getX(Hand.kRight)) > DEAD_ZONE) {
+driveX = xboxController.getX(Hand.kRight) * TURN_SCALE;
+turningPressed = false;
+} else {
+driveX = 0.0;
+turningPressed = false;
+}
+
+if (xboxController.getAButton()) {
+gyroOffset = gyro.get();
+m_drivetrain.resetEncoders();
+}
+
+if (xboxController.getPOV() == 0) {
+gyroTurningScale += 0.01;
+} else if (xboxController.getPOV() == 180) {
+gyroTurningScale -= 0.01;
+}
+
+m_drivetrain.arcadeDrive(driveY, driveX);
+}
 
   /** This function is called once when the robot is disabled. */
   @Override
@@ -99,4 +245,82 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
+
+public double getGyroAngle() {
+return gyro.get() - gyroOffset;
+}
+
+public void drive(double inches) {
+if (inches < 0) {
+if (getAvgEncoder() < (inches + 3)) {
+driveY = 0.0;
+
+Timer.delay(0.1);
+
+//speed of the robot for auto
+m_drivetrain.resetEncoders();
+autoStage++;
+} else {
+if (driveY > -0.8) {
+driveY -= 0.1;
+}
+}
+} else {
+if (getAvgEncoder() > (inches - 3)) {
+driveY = 0.0;
+
+Timer.delay(0.1);
+
+m_drivetrain.resetEncoders();
+autoStage++;
+} else {
+if (driveY < 0.8) {
+driveY += 0.1;
+}
+}
+}
+
+driveX = -getGyroAngle() / 15.0;
+
+if (driveX < -0.2) {
+driveX = -0.2;
+} else if (driveX > 0.2) {
+driveX = 0.2;
+}
+}
+
+public void turn(double degrees) {
+driveY = 0;
+
+if (degrees < 0) {
+if ((getGyroAngle() - (degrees + 30)) < 0.0) {
+driveX = 0.0;
+
+Timer.delay(0.1);
+
+//auto turn speed
+m_drivetrain.resetEncoders();
+autoStage++;
+gyroOffset += degrees;
+} else {
+driveX = -0.4;
+}
+} else {
+if ((getGyroAngle() - (degrees - 30)) > 0.0) {
+driveX = 0.0;
+
+Timer.delay(0.1);
+
+m_drivetrain.resetEncoders();
+autoStage++;
+gyroOffset += degrees;
+} else {
+driveX = 0.4;
+}
+}
+}
+
+public double getAvgEncoder() {
+return (leftEncoder + rightEncoder) / 2.0;
+}
 }
